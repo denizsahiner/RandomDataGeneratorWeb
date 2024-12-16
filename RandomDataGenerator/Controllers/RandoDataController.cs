a84d9015190a458ce1a6cd81e0b5124c7419823c
denizsahiner <dksahiner@gmail.com>
2024-12-16 03:06:25 +0300

    ilk commit

---
+++ RandomDataGenerator/Controllers/RandoDataController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RandomDataGenerator.Controllers
+{
+    public class RandomDataController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+
+}
---
+++ RandomDataGenerator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RandomDataGenerator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
