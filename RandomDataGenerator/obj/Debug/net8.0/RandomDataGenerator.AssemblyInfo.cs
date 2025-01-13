bf8adae28353d9cbc0dd11770199d4051addbd5c
denizsahiner <dksahiner@gmail.com>
2025-01-13 17:44:59 +0300

    Arayüz düzeltmeleri yapıldı.

--- RandomDataGenerator/obj/Debug/net8.0/RandomDataGenerator.AssemblyInfo.cs
+++ RandomDataGenerator/obj/Debug/net8.0/RandomDataGenerator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RandomDataGenerator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ddf17192974bf4a311ac3b61b77aa493283a775")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c758bd1536338f55239834fdc1d0f6eab90ded2")]
 [assembly: System.Reflection.AssemblyProductAttribute("RandomDataGenerator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RandomDataGenerator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
